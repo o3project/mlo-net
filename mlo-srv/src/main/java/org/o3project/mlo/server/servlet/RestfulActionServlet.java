372951700c111f8059eb7746ca2444d3cded848c
penguinwatcher@gmail.com <penguinwatcher@gmail.com>
2015-04-01 11:21:26 +0900

    Appends PUT and DELETE method to mlo-srv.
    as follows:
    * Appends RestfulActionServlet.java
    * Modifies action servlet in web.xml

---
+++ mlo-srv/src/main/java/org/o3project/mlo/server/servlet/RestfulActionServlet.java
@@ -0,0 +1,38 @@
+/**
+ * RestfulActionServlet.java
+ * (C) 2015, Hitachi, Ltd.
+ */
+package org.o3project.mlo.server.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionServlet;
+
+/**
+ * RestfulActionServlet
+ *
+ */
+public class RestfulActionServlet extends ActionServlet {
+
+	private static final long serialVersionUID = 3712488150972991853L;
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPut(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		process(req, res);
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doDelete(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		process(req, res);
+	}
+}
