ab96fb52616facd3997bdcca7b1229311ff5073c
penguinwatcher <penguinwatcher@gmail.com>
2015-07-15 04:59:19 +0000

    Changes the way to setup tomcat in setup.sh
    * To use bin/tomcat.setup.sh

--- setup.sh
+++ setup.sh
@@ -16,13 +16,6 @@ sudo install --mode=755 conf/$REDIS_CONF /$REDIS_CONF
 sudo service redis-server restart
 
 # for mlo
-sudo apt-get install tomcat7 tomcat7-admin -y
-TOMCAT_USERS_XML=var/lib/tomcat7/conf/tomcat-users.xml
-sudo install --mode=644 --owner=root --group=tomcat7 conf/$TOMCAT_USERS_XML /$TOMCAT_USERS_XML
-CRON_TOMCAT=etc/logrotate.d/tomcat7
-sudo install --mode=644 --owner=root --group=root conf/$CRON_TOMCAT /$CRON_TOMCAT
-DEFAULT_TOMCAT=etc/default/tomcat7
-sudo install --mode=644 --owner=root --group=root conf/$DEFAULT_TOMCAT /$DEFAULT_TOMCAT
-sudo service tomcat7 restart
+./bin/tomcat.setup.sh
 
 
