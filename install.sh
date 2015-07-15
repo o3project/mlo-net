575eebd5a0393762e30480698df738a6a71c6f44
penguinwatcher <penguinwatcher@gmail.com>
2015-07-15 16:51:49 +0900

    Changes install.sh to mitigate execution time.
    * Uses "tomcat7:redeploy-only" to avoid building mlo-srv twice

--- install.sh
+++ install.sh
@@ -7,10 +7,13 @@ JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 git clone -b 1.0.0 https://github.com/o3project/odenos.git odenos-1.0.0
 mvn clean package $M2_EXTRA_OPTS -f odenos-1.0.0/pom.xml
 
-# Deploys MLO
+# Builds MLO
 mvn clean install -Pproduct $M2_EXTRA_OPTS -f mlo-srv/pom.xml
-mvn clean tomcat7:redeploy $M2_EXTRA_OPTS -f mlo-psdtnc/pom.xml
-mvn clean tomcat7:redeploy -Pproduct $M2_EXTRA_OPTS -f mlo-srv/pom.xml
+mvn clean install $M2_EXTRA_OPTS -f mlo-psdtnc/pom.xml
+
+# Deploys MLO
+mvn tomcat7:redeploy-only $M2_EXTRA_OPTS -f mlo-psdtnc/pom.xml
+mvn tomcat7:redeploy-only $M2_EXTRA_OPTS -f mlo-srv/pom.xml
 
 sh system.restart.sh
 
