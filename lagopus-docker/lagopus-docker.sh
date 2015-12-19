acfe40983cc61914f8dd57df05174b64a618bee0
hinouchi <hinouchi@cocoa.plala.or.jp>
2015-12-20 02:39:40 +0900

    docker.io chosen instead of lxc-docker-xx

--- lagopus-docker/lagopus-docker.sh
+++ lagopus-docker/lagopus-docker.sh
@@ -108,10 +108,11 @@ case "$1" in
 	;;
     install)
 	check_user
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-	sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+	#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+	#sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 	sudo apt-get update
-	sudo apt-get install -y --force-yes lxc-docker-1.9.1
+	#sudo apt-get install -y --force-yes lxc-docker-1.9.1
+	sudo apt-get install -y --force-yes docker.io
 	sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
 	sudo gpasswd -a `whoami` docker
 	sudo apt-get install -y --force-yes iputils-arping
