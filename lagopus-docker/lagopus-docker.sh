8889facb01909509a9f845db1acae234d77a1219
hinocuhi <hinouchi@cocoa.plala.or.jp>
2016-03-30 16:02:29 +0900

    sync update with dev-v02

--- lagopus-docker/lagopus-docker.sh
+++ lagopus-docker/lagopus-docker.sh
@@ -21,6 +21,22 @@ extract_tag() {
     fi
 }
 
+docker_build_ryu() {
+    #sudo docker pull osrg/ryu
+    sudo docker pull ubuntu:14.04
+    sudo docker run --name ryu-p0 -it ubuntu:14.04 bash -c "cd /root && apt-get update -y && apt-get install build-essential zlib1g-dev libssl-dev git-core wget -y && wget http://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz && tar xzvf Python-2.7.9.tgz && (cd Python-2.7.9; ./configure --enable-shared --with-threads --enable-unicode=ucs4; make && make install; ldconfig -v) && git clone -b v3.29 https://github.com/osrg/ryu && ln -sf /root/ryu /root/ryu-master && wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py"
+    sudo docker commit ryu-p0 ryu:lldp
+    sudo docker run --name ryu-p1 -it ryu:lldp sed -i -e "s/^LLDP_MAC_NEAREST_BRIDGE/###LLDP_MAC_NEAREST_BRIDGE/" /root/ryu-master/ryu/lib/packet/lldp.py
+    sudo docker commit ryu-p1 ryu:lldp
+    sudo docker run --name ryu-p2 -it ryu:lldp sed -i -e "/^###LLDP_MAC_NEAREST_BRIDGE/i LLDP_MAC_NEAREST_BRIDGE = 'ff:ff:ff:ff:ff:ff'" /root/ryu-master/ryu/lib/packet/lldp.py
+    sudo docker commit ryu-p2 ryu:lldp
+    sudo docker run --name ryu-p3 -it ryu:lldp bash -c "cd /root/ryu-master; python ./setup.py install"
+    sudo docker commit ryu-p3 ryu:lldp
+    sudo docker run --name ryu-p4 -it ryu:lldp bash -c "pip install webob routes 'oslo.config==3.0.0' msgpack-python eventlet"
+    sudo docker commit ryu-p4 ryu:lldp
+    sudo docker rm -f ryu-p0 ryu-p1 ryu-p2 ryu-p3 ryu-p4
+}
+
 docker_build_lagopus_vswitch() {
     extract_tag
     sudo docker build --no-cache -t lagopus-vswitch:${TAG} .
@@ -108,26 +124,18 @@ case "$1" in
 	;;
     install)
 	check_user
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-	sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+	#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+	#sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 	sudo apt-get update
-	sudo apt-get install -y --force-yes lxc-docker-1.8.1
+	#sudo apt-get install -y --force-yes lxc-docker-1.9.1
+	sudo apt-get install -y --force-yes docker.io
 	sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
 	sudo gpasswd -a `whoami` docker
 	sudo apt-get install -y --force-yes iputils-arping
 	sudo apt-get install -y --force-yes bridge-utils
 	sudo apt-get install -y --force-yes curl
 	sudo service docker restart
-	sudo docker pull osrg/ryu
-	sudo docker run --name ryu-p1 -it osrg/ryu sed -i -e "s/^LLDP_MAC_NEAREST_BRIDGE/###LLDP_MAC_NEAREST_BRIDGE/" /root/ryu-master/ryu/lib/packet/lldp.py
-	sudo docker commit ryu-p1 ryu:lldp
-	sudo docker run --name ryu-p2 -it ryu:lldp sed -i -e "/^###LLDP_MAC_NEAREST_BRIDGE/i LLDP_MAC_NEAREST_BRIDGE = 'ff:ff:ff:ff:ff:ff'" /root/ryu-master/ryu/lib/packet/lldp.py
-	sudo docker commit ryu-p2 ryu:lldp
-	sudo docker run --name ryu-p3 -it ryu:lldp bash -c "cd /root/ryu-master; python ./setup.py install"
-	sudo docker commit ryu-p3 ryu:lldp
-	sudo docker run --name ryu-p4 -it ryu:lldp bash -c "pip install netaddr six pbr --upgrade"
-	sudo docker commit ryu-p4 ryu:lldp
-	sudo docker rm -f ryu-p1 ryu-p2 ryu-p3 ryu-p4
+	docker_build_ryu
 	docker_build_lagopus_vswitch
 	mod_sudoers
 	;;
